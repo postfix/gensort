f36b2315e7215005de41f7caa7c2bbc57b7f55e6
mozu0 <mozu.oishi@gmail.com>
2015-02-06 05:34:07 +0000

    Add sample usage.

---
+++ sample/a.go
@@ -0,0 +1,11 @@
+package main
+
+//go:generate gensort --output sort.int64.go --type int64 --package main --name sortInt64 --less "x > y" --eq "x == y"
+
+import "fmt"
+
+func main() {
+	xs := []int64{3, 1, 2}
+	sortInt64(xs)
+	fmt.Println(xs)
+}
